e27ec3e835f469f06011a0af3ca85578cda82f01
Ana Torres <anasofitorresbe@gmail.com>
2024-08-11 12:48:21 -0400

    got rid of gnome completely for real now

---
+++ home/cosmic/.#deconf.nix
@@ -0,0 +1,1 @@
+abacaxinera@poderoso.73753:1723389158
--- home/cosmic/default.nix
+++ home/cosmic/default.nix
@@ -3,18 +3,8 @@
 {
   # imports = [ ./dconf.nix ];
 
-  programs.gnome-shell = {
-    enable = true;
-    extensions = [
-      { package = pkgs.gnomeExtensions.pop-shell; }
-      { package = pkgs.gnomeExtensions.appindicator; }
-      { package = pkgs.gnomeExtensions.vitals; }
-    ];
-  };
-
   home.packages = with pkgs; [
-    gnome.gnome-tweaks
-    gnome-console
-    gnome.nautilus
+    #
   ];
+  
 }
