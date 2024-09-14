5d0d4d98a09affa0fb8b250c7e98dde932497b1c
Ana Torres <anasofitorresbe@gmail.com>
2024-09-13 20:25:58 -0400

    moves eduroam

--- home/connections/eduroam/default.nix
+++
@@ -1,7 +0,0 @@
-{pkgs, ...}:
-
-{
-  home.packages = with pkgs; [
-    wpa_supplicant
-  ];
-}
--- home/connections/fortivpn/connect.fish
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env fish
-
-openfortivpn-webview --url 'https://epxvpn.ciena.com/remote/saml/start?realm=partner' --trusted-cert='sha256/HJdEvkbyTW9ygH0azudaf8VN2Fuvl8odWWghLPX+T6c=' 2>/dev/null | sudo openfortivpn "epxvpn.ciena.com:443" --cookie-on-stdin --pppd-accept-remote --trusted-cert aa8c394f40777bf4a9908819ea9799826020470ca8156376762d9402baa96c34
